67344bd29f5f9187f95c9499a77b0f9ff51bd2e3
Martin Colbourne <martinlambc@gmail.com>
2026-02-06 15:01:50 +0000

    chore: adds files and app.py boilerplate

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from footeh!")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+db.init_app(app)
 
 
-if __name__ == "__main__":
-    main()
+
---
+++ models.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import db
+
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64))
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return f'User: {self.name}, ID: {self.id}, email: {self.email}'
