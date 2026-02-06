20bffc3bdeeca19b70323d92503fcc7d55a95870
Martin Colbourne <martinlambc@gmail.com>
2026-02-06 15:52:11 +0000

    chore: change structure to use app module

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
@@ -10,10 +11,11 @@ load_dotenv()
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-
-
+from app import routes, models
