4b911acf40a6027e321ebb6f86b20f7ce878ba7e
Martin Colbourne <martinlambc@gmail.com>
2026-03-21 20:01:49 +0000

    add basic register route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Fancy a game of footeh?"
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register_get():
+    return render_template("register.html")
