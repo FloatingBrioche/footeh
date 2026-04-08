5d3fff1623d39a901fa3c8632098bcadd161852a
Martin Colbourne <martinlambc@gmail.com>
2026-04-08 23:00:51 +0100

    add logic for login and registration

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,10 @@ from flask import render_template, redirect, session, request
 from werkzeug.security import check_password_hash
 
 
-from app import app
+from app import app, db
 from app.utils import login_required, validate_form
 from app.validators import Registration
+from app.db_models import User, Group, Membership, League, Game, Appearance
 
 
 # <------------------------------------------------------ Home
@@ -29,7 +30,14 @@ def register_get():
 @app.route("/register", methods=["POST"])
 @validate_form(validator=Registration, template="register.html")
 def register_post(registration_data: Registration):
-    pass
+    new_user = User(**registration_data.model_dump())
+    db.session.add(new_user)
+    db.session.commit()
+    session["user_id"] = new_user.id
+    session["first_name"] = new_user.first_name
+    session["last_name"] = new_user.last_name
+    session["email"] = new_user.email
+    return redirect("/groups")
 
 
 @app.route("/login", methods=["GET"])
@@ -38,6 +46,29 @@ def login_get():
         return redirect("/games")
     else:
         return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get("email", "").strip()
+    password = request.form.get("password", "")
+    
+    user = db.session.execute(db.select(User).filter_by(email=email)).scalar_one_or_none()
+    
+    if not user:
+        error_message = "Invalid email. There's no account associated with that email address."
+        return render_template("login.html", error=error_message)
+
+    if not check_password_hash(user.password_hash, password):
+        error_message = "Invalid password."
+        return render_template("login.html", error=error_message)
+    
+    session["user_id"] = user.id
+    session["first_name"] = user.first_name
+    session["last_name"] = user.last_name
+    session["email"] = user.email
+    
+    return redirect("/games")
 
 
 @app.route("/logout", methods=["GET"])
