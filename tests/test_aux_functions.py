721dc9c1425b7f8ab34fbfde1e46219bf3e3e84c
Martin Colbourne <martinlambc@gmail.com>
2026-04-16 18:04:24 +0100

    add is_unique_jcode test

---
+++ tests/test_aux_functions.py
@@ -0,0 +1,16 @@
+from pytest import mark, raises, fixture
+
+
+from utils.aux_functions import generate_join_code, is_jcode_unique
+
+class TestGenerateJoinCode:
+    @mark.it("Generates join code <= 30 words")
+    def test_code_le_30(self):
+        join_code = generate_join_code()
+        assert len(join_code) <= 30
+
+
+class TestIsUniqueJCode:
+    @mark.it("Returns True if jcode not in current jcodes")
+    def test_returns_true(self, mock_db):
+        assert is_jcode_unique("eggy-egg-egg", mock_db)
