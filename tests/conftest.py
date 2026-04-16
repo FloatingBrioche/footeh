11b13d2fa62f3c980fc5b33d14af63017eb13e02
Martin Colbourne <martinlambc@gmail.com>
2026-04-16 18:03:44 +0100

    add mock_db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,16 @@
 import os
 import sys
+from unittest.mock import Mock
+
+from pytest import fixture
+
 
 cwd = os.getcwd()
 sys.path.insert(0, cwd)
+
+
+@fixture
+def mock_db():
+    db = Mock()
+    db.session.execute.return_value = ["happy-dog-bog", "soft-slimey-tractor"]
+    return db
