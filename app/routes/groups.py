c6368a523f284cb8a7d67af2db53f54e9fa7dc8f
Martin Colbourne <martinlambc@gmail.com>
2026-04-16 11:18:38 +0100

    add basic groups_join and _new routes

--- app/routes/groups.py
+++ app/routes/groups.py
@@ -5,3 +5,15 @@ from app.routes import *
 @login_required
 def groups_get():
     return render_template("groups.html")
+
+
+@app.get("groups/new")
+@login_required
+def groups_new_get():
+    render_template("group_new.html")
+
+
+@app.get("groups/join")
+@login_required
+def groups_new_get():
+    render_template("group_join.html")
