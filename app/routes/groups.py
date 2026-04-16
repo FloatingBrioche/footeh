c0ecac9484714a50be658a8cd6e89e8485cd6212
Martin Colbourne <martinlambc@gmail.com>
2026-04-16 14:36:26 +0100

    fix endpoints

--- app/routes/groups.py
+++ app/routes/groups.py
@@ -7,13 +7,20 @@ def groups_get():
     return render_template("groups.html")
 
 
-@app.get("groups/new")
+@app.post("/groups")
+@login_required
+@validate_form(validator=NewGroup, template="groups_new.html")
+def groups_post():
+    return render_template("groups.html")
+
+
+@app.get("/groups/new")
 @login_required
 def groups_new_get():
     render_template("group_new.html")
 
 
-@app.get("groups/join")
+@app.get("/groups/join")
 @login_required
-def groups_new_get():
+def groups_join_get():
     render_template("group_join.html")
